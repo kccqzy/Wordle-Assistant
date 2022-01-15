c41ced0aeda441c1450c87b76b2a643536231f77
Zhouyu Qian <qzy@qzy.io>
2022-01-14 22:17:17 -0800

    Refactored code and added functionality for evaluating subsequent guesses

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,22 @@ impl Display for Word {
     }
 }
 
+impl TryFrom<&str> for Word {
+    type Error = &'static str;
+    fn try_from(value: &str) -> std::result::Result<Self, Self::Error> {
+        let bytes = value.as_bytes();
+        if !bytes.is_ascii() {
+            return Err("not an ASCII word");
+        }
+        let mut word: [u8; 5] = bytes.try_into().map_err(|_| "not a five-letter word")?;
+        word.make_ascii_lowercase();
+        if !word.iter().all(|&c| (b'a'..=b'z').contains(&c)) {
+            return Err("not all ASCII letters");
+        }
+        Ok(Word(word))
+    }
+}
+
 type Words = Vec<Word>;
 
 type Result<T> = std::result::Result<T, String>;
@@ -28,6 +44,7 @@ enum GuessLetterResult {
 
 type GuessWordResult = [GuessLetterResult; 5];
 
+#[derive(Clone)]
 struct GuessState {
     letter_choices: [u32; 5],
     must_appear: u32,
@@ -36,6 +53,21 @@ struct GuessState {
 impl Default for GuessState {
     fn default() -> Self {
         GuessState { letter_choices: [(1 << 26) - 1; 5], must_appear: 0 }
+    }
+}
+
+impl std::fmt::Debug for GuessState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        fn set_to_str(s: u32) -> String {
+            BitIter::from(s).map(|c| (c as u8 + b'a') as char).collect()
+        }
+        f.debug_struct("GuessState")
+            .field(
+                "letter_choices",
+                &self.letter_choices.iter().copied().map(set_to_str).collect::<Vec<String>>(),
+            )
+            .field("must_appear", &set_to_str(self.must_appear))
+            .finish()
     }
 }
 
@@ -69,6 +101,12 @@ impl GuessState {
         }
     }
 
+    fn then(&self, guessed: Word, gwr: GuessWordResult) -> Self {
+        let mut new = self.clone();
+        new.update(guessed, gwr);
+        new
+    }
+
     fn is_word_possible(&self, w: Word) -> bool {
         w.0.iter().zip(self.letter_choices.iter()).all(|(&c, &p)| (p & (1 << (c - b'a'))) != 0)
             && BitIter::from(self.must_appear).all(|c| w.0.iter().any(|&ch| ch == c as u8 + b'a'))
@@ -87,21 +125,11 @@ fn load_words() -> Result<Words> {
         if line.starts_with('#') {
             continue;
         }
-        let line_bytes = line.as_bytes();
-        if !line_bytes.is_ascii() {
-            return Err(format!("File contains word {:?} which is not an ASCII word", line));
-        }
-        let mut word: [u8; 5] = line_bytes.try_into().map_err(|_| {
-            format!("File contains word {:?} which is not a five-letter word", line)
-        })?;
-        word.make_ascii_lowercase();
-        if !word.iter().all(|&c| (b'a'..=b'z').contains(&c)) {
-            return Err(format!(
-                "File contains word {:?} which is not composed of all letters",
-                line
-            ));
-        }
-        rv.push(Word(word));
+        let line: &str = &line;
+        let word: Word = line
+            .try_into()
+            .map_err(|e| format!("File contains word {:?} which is invalid: {}", line, e))?;
+        rv.push(word);
     }
     Ok(rv)
 }
@@ -124,8 +152,8 @@ fn process_guess(guessed: Word, actual: Word) -> GuessWordResult {
     rv
 }
 
-// Returns the quality of a guess. A high quality guess eliminates all but one word and has a quality of 1. A low quality guess eliminates nothing and has a quality of 0.
-fn initial_guess_quality(guess_state: &GuessState, words: &Words) -> f64 {
+// Returns the quality of a guess state. A high quality guess eliminates all but one word and has a quality of 1. A low quality guess eliminates nothing and has a quality of 0.
+fn quality(guess_state: &GuessState, words: &Words) -> f64 {
     let total = words.len() as f64;
     let remaining = words.iter().filter(|&&w| guess_state.is_word_possible(w)).count() as f64;
 
@@ -138,20 +166,27 @@ fn initial_guess_quality(guess_state: &GuessState, words: &Words) -> f64 {
     (total - remaining) / (total - 1.0)
 }
 
-fn find_initial_guess(words: &Words) -> Word {
+fn guess_quality(s: &GuessState, guessed_word: Word, words: &Words) -> f64 {
+    words
+        .iter()
+        .map(|&actual_word| {
+            quality(&s.then(guessed_word, process_guess(guessed_word, actual_word)), words)
+        })
+        .sum::<f64>()
+        / words.len() as f64
+}
+
+fn find_best_guess(s: &GuessState, words: &Words) -> Word {
     // For each guessed word, we evaluate for each possible actual word, the guess quality.
+    let words = &words
+        .iter()
+        .filter(|&&actual_word| s.is_word_possible(actual_word))
+        .cloned()
+        .collect::<Words>();
     *words
         .iter()
         .max_by_key(|&&guessed_word| {
-            let rv = words
-                .iter()
-                .map(|&actual_word| {
-                    let mut guess_state = GuessState::default();
-                    guess_state.update(guessed_word, process_guess(guessed_word, actual_word));
-                    initial_guess_quality(&guess_state, words)
-                })
-                .sum::<f64>() as f64
-                / words.len() as f64;
+            let rv = guess_quality(s, guessed_word, words);
             println!("word = {} quality = {:.6}", guessed_word, rv);
             (rv * 1e6) as u64
         })
@@ -161,7 +196,17 @@ fn find_initial_guess(words: &Words) -> Word {
 fn real_main() -> Result<()> {
     let words = load_words()?;
     eprintln!("Loaded {} words", words.len());
-    eprintln!("Initial Guess: {}", find_initial_guess(&words));
+    // eprintln!("Initial Guess: {}", find_best_guess(&GuessState::default(), &words));
+    let example_trace: &[(Word, GuessWordResult)] = &[("RAISE".try_into().unwrap(), [
+        GuessLetterResult::Grey,
+        GuessLetterResult::Green,
+        GuessLetterResult::Grey,
+        GuessLetterResult::Grey,
+        GuessLetterResult::Grey,
+    ])];
+    let example_state = &GuessState::default().then(example_trace[0].0, example_trace[0].1);
+    eprintln!("State after first trial: {:?}", example_state);
+    eprintln!("First trial: {}", find_best_guess(&example_state, &words));
     Ok(())
 }
 
